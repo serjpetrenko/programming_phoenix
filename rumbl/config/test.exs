cfb5f7a39cabffe9810bf3065b90423991abcf61
serjpetrenko <b.g.lupus@gmail.com>
2022-04-18 20:02:23 +0300

    Add tests to Auth and views

--- rumbl/config/test.exs
+++ rumbl/config/test.exs
@@ -28,3 +28,6 @@ config :logger, level: :warn
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+# Rounds password hashes to improve test speed
+config :pbkdf2_elixir, :rounds, 1
